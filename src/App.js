d115fbf3ac7288599c95648536f96cffee750ab7
Gustavo Chiclla <gustavohoper@gmail.com>
2021-06-17 21:39:21 -0500

    props y useState

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Input from './components/Input/index'
 
 import logo from './logo.svg'
 import './App.css';
+import Button from './components/Button';
 
 const Test = ({ children }) => {
   return (
     <div>
-      Test 
+      Test
       {children}
     </div>
   )
@@ -20,6 +21,7 @@ function App() {
   const [apellido, setApellido] = useState('')
   const [email, setEmail] = useState('')
   const [dni, setDni] = useState('')
+  const [count, setCount] = useState(0)
 
   const handleShowImage = () => {
     setShow((prevState) => !prevState)
@@ -29,6 +31,11 @@ function App() {
     console.log('e', e.target.value)
     cb(e.target.value)
   }
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  console.log(count);
 
   return (
     <div className="App">
@@ -54,6 +61,14 @@ function App() {
           value={dni}
         />
       </Test>
+
+      <Button
+        label="Gustavo"
+        onClick={handleClick}
+        value={count}
+      />
+
+      <div>{count}</div>
       <button onClick={handleShowImage} >{show ? 'Ocultar' : 'Mostrar'} imagen</button>
       {show && (
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = (props) => {
+  const { label, onClick } = props
+  return (
+    <button onClick={onClick} >{label}</button>
+  )
+}
+
+export default Button
